a73b8b71cdb0b059ef56944d5c122af4300b0d04
Hchan2 <seongchan_@naver.com>
2025-10-23 17:43:34 +0900

    feat(App): '모든 클래스를 적용한 게임 진행' User, Valid, Game 클래스를 활용한 전체 게임을 진행합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import Game from "./Game";
+import User from "./User";
+import Valid from "./Valid";
+
 class App {
-  async run() {}
+  async run() {
+    await this.user();
+  }
+
+  async user() {
+    const user = new User();
+
+    await user.inputCarName();
+    await user.inputTryCount();
+
+    const carNames = user.refinedName;
+    const tryCount = user.tryCountValue;
+
+    await this.valid(carNames, tryCount);
+  }
+
+  async valid(carNames, tryCount) {
+    const valid = new Valid(carNames, tryCount);
+
+    await valid.isValid();
+
+    await this.game(carNames, tryCount);
+  }
+
+  async game(carNames, tryCount) {
+    const game = new Game(carNames, tryCount);
+
+    await game.game();
+    await game.result();
+  }
 }
 
 export default App;
