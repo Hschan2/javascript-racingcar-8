0f4ad988a54a378f64b304c420033188d77f53d6
Hchan2 <seongchan_@naver.com>
2025-10-24 21:18:31 +0900

    feat(GameTest): '수정된 Game 클래스에 맞게 테스트 코드 수정' Game 클래스의 수정된 코드에 맞게 Game Test 코드 수정합니다.

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -15,103 +15,82 @@ const getLogSpy = () => {
 };
 
 describe("Game 클래스 테스트", () => {
-  test("constructor: 참가자를 올바르게 초기화해야 한다", () => {
+  test("constructor: 참가자 올바르게 초기화", () => {
     const game = new Game(["pobi", "woni"], "5");
     expect(game.participants).toEqual({ pobi: "", woni: "" });
   });
 
   describe("자동차 전진 로직 (_moveCars)", () => {
-    test("랜덤 숫자가 4 이상일 때 전진해야 한다", () => {
-      // given
+    test("랜덤 숫자가 4 이상일 때 전진", () => {
       mockRandoms([4, 3]);
       const game = new Game(["pobi", "woni"], "1");
 
-      // when
       game._moveCars();
 
-      // then
       expect(game.participants.pobi).toBe("-");
       expect(game.participants.woni).toBe("");
     });
 
-    test("랜덤 숫자가 4 미만일 때 멈춰야 한다", () => {
-      // given
+    test("랜덤 숫자가 4 미만일 때 그대로 유지", () => {
       mockRandoms([0, 9]);
       const game = new Game(["pobi", "woni"], "1");
 
-      // when
       game._moveCars();
 
-      // then
       expect(game.participants.pobi).toBe("");
       expect(game.participants.woni).toBe("-");
     });
   });
 
   describe("우승자 계산 로직 (_getWinners)", () => {
-    test("단독 우승자가 있을 경우 해당 우승자만 배열로 반환해야 한다", () => {
-      // given
+    test("단독 우승자가 있을 경우 해당 우승자만 배열로 반환", () => {
       const game = new Game(["pobi", "woni"], "1");
       game.participants = { pobi: "--", woni: "-" };
 
-      // when
       const winners = game._getWinners();
 
-      // then
       expect(winners).toEqual(["pobi"]);
     });
 
-    test("공동 우승자가 있을 경우 모든 우승자를 배열로 반환해야 한다", () => {
-      // given
+    test("공동 우승자가 있을 경우 모든 우승자를 배열로 반환", () => {
       const game = new Game(["pobi", "woni", "jun"], "1");
       game.participants = { pobi: "--", woni: "-", jun: "--" };
 
-      // when
       const winners = game._getWinners();
 
-      // then
       expect(winners).toEqual(["pobi", "jun"]);
     });
 
-    test("아무도 전진하지 않았을 경우 빈 배열을 반환해야 한다", () => {
-      // given
+    test("아무도 전진하지 않았을 경우 빈 배열 반환", () => {
       const game = new Game(["pobi", "woni"], "1");
       game.participants = { pobi: "", woni: "" };
 
-      // when
       const winners = game._getWinners();
 
-      // then
       expect(winners).toEqual([]);
     });
   });
 
   describe("우승자 출력 (printWinners)", () => {
-    test("계산된 우승자들을 형식에 맞게 올바르게 출력해야 한다", () => {
-      // given
+    test("계산된 우승자들을 형식에 맞게 올바르게 출력", () => {
       const logSpy = getLogSpy();
       const game = new Game(["pobi", "woni", "jun"], "1");
       game.participants = { pobi: "--", woni: "-", jun: "--" };
 
-      // when
       game.printWinners();
 
-      // then
       expect(logSpy).toHaveBeenCalledWith("최종 우승자 : pobi, jun");
     });
   });
 
   describe("전체 게임 흐름 테스트 (play)", () => {
-    test("게임이 시작부터 끝까지 올바르게 동작하고 결과를 출력해야 한다", async () => {
-      // given
-      mockRandoms([9, 0, 8, 1]); // pobi: 전진, woni: 멈춤, pobi: 전진, woni: 멈춤
+    test("게임이 시작부터 끝까지 올바르게 동작하고 결과 출력", async () => {
+      mockRandoms([9, 0, 8, 1]);
       const logSpy = getLogSpy();
       const game = new Game(["pobi", "woni"], "2");
 
-      // when
       await game.play();
 
-      // then
       const expectedFinalState = { pobi: "--", woni: "" };
       expect(game.participants).toEqual(expectedFinalState);
 
