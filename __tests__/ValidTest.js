673644c5a886c104ab7ac494b0beb941af53de88
Hchan2 <seongchan_@naver.com>
2025-10-24 21:19:23 +0900

    feat(ValidTest): '변경된 함수 이름 적용' Valid 클래스에서 변경된 함수의 이름을 사용하기 위해 수정합니다.

--- __tests__/ValidTest.js
+++ __tests__/ValidTest.js
@@ -1,39 +1,39 @@
 import Valid from "../src/Valid.js";
 
 describe("Valid 클래스 테스트", () => {
-  describe("isNotThingCarName: 자동차 이름 입력 유효성 검사", () => {
+  describe("validateCarNameIsNotEmpty: 자동차 이름 입력 유효성 검사", () => {
     test("자동차 이름이 빈 배열일 경우 에러", async () => {
       const valid = new Valid([], "5");
-      await expect(valid.isNotThingCarName()).rejects.toThrow("[ERROR] 자동차 이름을 입력하지 않았습니다.");
+      await expect(valid.validateCarNameIsNotEmpty()).rejects.toThrow("[ERROR] 자동차 이름을 입력하지 않았습니다.");
     });
 
     test("자동차 이름이 비어있지 않으면 에러 없음", async () => {
       const valid = new Valid(["pobi", "woni"], "5");
-      await expect(valid.isNotThingCarName()).resolves.not.toThrow();
+      await expect(valid.validateCarNameIsNotEmpty()).resolves.not.toThrow();
     });
   });
 
-  describe("isOverNameLength: 자동차 이름 길이 유효성 검사", () => {
+  describe("validateCarNameLength: 자동차 이름 길이 유효성 검사", () => {
     test("자동차 이름이 5자를 초과할 경우 에러", async () => {
       const valid = new Valid(["pobi", "javaji"], "5");
-      await expect(valid.isOverNameLength()).rejects.toThrow("[ERROR] 자동차 이름이 5글자를 초과합니다.");
+      await expect(valid.validateCarNameLength()).rejects.toThrow("[ERROR] 자동차 이름이 5글자를 초과합니다.");
     });
 
     test("모든 자동차 이름이 5자 이하일 경우 에러 없음", async () => {
       const valid = new Valid(["pobi", "woni"], "5");
-      await expect(valid.isOverNameLength()).resolves.not.toThrow();
+      await expect(valid.validateCarNameLength()).resolves.not.toThrow();
     });
   });
 
-  describe("isNotThingTryCount: 시도 횟수 입력 유효성 검사", () => {
+  describe("validateTryCountIsNotEmpty: 시도 횟수 입력 유효성 검사", () => {
     test("시도 횟수가 빈 문자열일 경우 에러", async () => {
       const valid = new Valid(["pobi", "woni"], "");
-      await expect(valid.isNotThingTryCount()).rejects.toThrow("[ERROR] 시도할 횟수를 입력하지 않았습니다.");
+      await expect(valid.validateTryCountIsNotEmpty()).rejects.toThrow("[ERROR] 시도할 횟수를 입력하지 않았습니다.");
     });
 
     test("시도 횟수가 비어있지 않으면 에러 없음", async () => {
       const valid = new Valid(["pobi", "woni"], "5");
-      await expect(valid.isNotThingTryCount()).resolves.not.toThrow();
+      await expect(valid.validateTryCountIsNotEmpty()).resolves.not.toThrow();
     });
   });
 
