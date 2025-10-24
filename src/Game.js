3d7a6b8ac88de6cf5a40261ea801765be627b9b6
Hchan2 <seongchan_@naver.com>
2025-10-24 18:18:07 +0900

    refactor(Game): '함수는 한 가지 일만 하도록 만들기' 각 함수(메소드)가 한 가지 일만 할 수 있도록 수정합니다.

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import { MissionUtils } from '@woowacourse/mission-utils'
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Game {
     constructor(carName, tryCount) {
@@ -7,38 +7,52 @@ class Game {
 
         carName.forEach(name => {
             this.participants[name] = '';
-        })
+        });
     }
 
-    async game() {
-        MissionUtils.Console.print("실행 결과");
+    async play() {
+        MissionUtils.Console.print('\n실행 결과');
         for (let i = 0; i < this.tryCount; i++) {
-            await this.racing();
+            this._moveCars();
+            this._printRoundStatus();
         }
+        this.printWinners();
     }
 
-    async racing() {
+    _moveCars() {
         const names = Object.keys(this.participants);
         names.forEach(name => {
             const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
             if (randomNum >= 4) {
                 this.participants[name] += '-';
             }
-            MissionUtils.Console.print(`${name} : ${this.participants[name]}`);
-        })
+        });
     }
 
-    async result() {
+    _printRoundStatus() {
+        const names = Object.keys(this.participants);
+        names.forEach(name => {
+            MissionUtils.Console.print(`${name} : ${this.participants[name]}`);
+        });
+        MissionUtils.Console.print('');
+    }
+
+    printWinners() {
+        const winners = this._getWinners();
+        MissionUtils.Console.print(`최종 우승자 : ${winners.join(', ')}`);
+    }
+
+    _getWinners() {
         const allLengths = Object.values(this.participants).map(track => track.length);
-        if (allLengths.length === 0) {
-        }
         const maxLength = Math.max(...allLengths);
 
-        const winners = Object.keys(this.participants).filter(name => {
+        if (maxLength === 0) {
+            return [];
+        }
+
+        return Object.keys(this.participants).filter(name => {
             return this.participants[name].length === maxLength;
         });
-
-        MissionUtils.Console.print(`최종 우승자 : ${winners.join(', ')}`);
     }
 }
 
