421fb54470af67897f502104d4154cd6d54a5c2d
Hchan2 <seongchan_@naver.com>
2025-10-24 21:20:17 +0900

    feat(App): 'run 메소드에 실행' 메소드로 분리된 과정을 run 메소드에서 실행하도록 변경합니다.

--- src/App.js
+++ src/App.js
@@ -4,34 +4,18 @@ import Valid from "./Valid";
 
 class App {
   async run() {
-    await this.user();
-  }
-
-  async user() {
     const user = new User();
-
     await user.inputCarName();
     await user.inputTryCount();
 
     const carNames = user.refinedName;
     const tryCount = user.tryCountValue;
 
-    await this.valid(carNames, tryCount);
-  }
-
-  async valid(carNames, tryCount) {
     const valid = new Valid(carNames, tryCount);
-
     await valid.isValid();
 
-    await this.game(carNames, tryCount);
-  }
-
-  async game(carNames, tryCount) {
     const game = new Game(carNames, tryCount);
-
-    await game.game();
-    await game.result();
+    await game.play();
   }
 }
 
