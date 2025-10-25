2c0f1f07da5ef4fc1ca1a8a95d1ef9c7cc099750
Hchan2 <seongchan_@naver.com>
2025-10-25 17:09:39 +0900

    refactor(Valid): 'async 제거' 비동기 함수를 동기 함수로 수정합니다.

--- src/Valid.js
+++ src/Valid.js
@@ -4,19 +4,19 @@ class Valid {
         this.tryCount = tryCount;
     }
 
-    async isValid() {
-        await this.validateCarNameIsNotEmpty();
-        await this.validateCarNameLength();
-        await this.validateTryCountIsNotEmpty();
+    isValid() {
+        this.validateCarNameIsNotEmpty();
+        this.validateCarNameLength();
+        this.validateTryCountIsNotEmpty();
     }
 
-    async validateCarNameIsNotEmpty() {
+    validateCarNameIsNotEmpty() {
         if (this.carName.length === 0) {
             throw new Error("[ERROR] 자동차 이름을 입력하지 않았습니다.");
         }
     }
 
-    async validateCarNameLength() {
+    validateCarNameLength() {
         for (const name of this.carName) {
             if (name.length > 5) {
                 throw new Error("[ERROR] 자동차 이름이 5글자를 초과합니다.");
@@ -24,7 +24,7 @@ class Valid {
         }
     }
 
-    async validateTryCountIsNotEmpty() {
+    validateTryCountIsNotEmpty() {
         if (this.tryCount.length === 0) {
             throw new Error("[ERROR] 시도할 횟수를 입력하지 않았습니다.");
         }
