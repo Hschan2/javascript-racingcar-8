2d9e175a5848b106cf32b35e1f8fa6e142ac1db9
Hchan2 <seongchan_@naver.com>
2025-12-08 16:52:36 +0900

    refactor: add a car name, try count new valid and use messages

--- src/Valid.js
+++ src/Valid.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from "./constant/message";
+
 class Valid {
     constructor(carName, tryCount) {
         this.carName = carName;
@@ -8,25 +10,39 @@ class Valid {
         this.validateCarNameIsNotEmpty();
         this.validateCarNameLength();
         this.validateTryCountIsNotEmpty();
+        this.validateTryCountIsNotPositiveNumber();
+        this.validateTryCountIsNotNumber();
     }
 
     validateCarNameIsNotEmpty() {
-        if (this.carName.length === 0) {
-            throw new Error("[ERROR] 자동차 이름을 입력하지 않았습니다.");
+        if (this.carName.length === 0 || this.carName.some(name => name.trim().length === 0)) {
+            throw new Error(ERROR_MESSAGE.VALID_CAR_NAME_NOT_EMPTY);
         }
     }
 
     validateCarNameLength() {
         for (const name of this.carName) {
             if (name.length > 5) {
-                throw new Error("[ERROR] 자동차 이름이 5글자를 초과합니다.");
+                throw new Error(ERROR_MESSAGE.VALID_CAR_NAME_LENGTH);
             }
         }
     }
 
     validateTryCountIsNotEmpty() {
         if (this.tryCount.length === 0) {
-            throw new Error("[ERROR] 시도할 횟수를 입력하지 않았습니다.");
+            throw new Error(ERROR_MESSAGE.VALID_TRY_COUNT_NOT_EMPTY);
+        }
+    }
+
+    validateTryCountIsNotPositiveNumber() {
+        if (Number(this.tryCount) === 0 || Number(this.tryCount) < 0) {
+            throw new Error(ERROR_MESSAGE.VALID_TRY_COUNT_NOT_POSITIVE_NUMBER);
+        }
+    }
+
+    validateTryCountIsNotNumber() {
+        if (!Number.isInteger(Number(this.tryCount))) {
+            throw new Error(ERROR_MESSAGE.VALID_TRY_COUNT_NOT_NUMBER);
         }
     }
 }
